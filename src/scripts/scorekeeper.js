b2a9bd0d326448434434225c69fd3baa3943f884
YWBK <ywbkdev@gmail.com>
2022-03-22 15:38:34 -0400

    refactor for linkedin

--- src/scripts/scorekeeper.js
+++ src/scripts/scorekeeper.js
@@ -13,11 +13,11 @@ export default class Scorekeeper {
         this.serveArr = [1,2];
         this.pointScore = {
             p1: 0,
-            p2: 0
+            p2: 3
         };
         this.gameScore = {
-            p1: 0,
-            p2: 0
+            p1: 6,
+            p2: 5
         };
         this.totalWinners = {
             p1: 0,
